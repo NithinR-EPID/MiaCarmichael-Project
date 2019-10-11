0743516bb40a211a94831aefa9698489bd0d1c19
Mia Carmichael <mnc24892@uga.edu>
2019-10-11 02:11:36 -0400

    project p ll edits

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -6,36 +6,33 @@
 
 #load needed packages. make sure they are installed.
 library(readxl)
+library(ggplot2)
 library(dplyr)
 
-#load data. path is relative to project directory.
-rawdata <- readxl::read_excel("./data/raw_data/exampledata.xlsx")
-
+#Load data, path is relative to project directory. Select a file and read the data into a data frame.
+rawdata <- readxl::read_excel("code/processing_code/Abundant_Phyla_MADA.xlsx")
+                                         
 #take a look at the data
+head(rawdata)
 dplyr::glimpse(rawdata)
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#Rename the columes, so when creating a graph things can be plotted easier
+names(rawdata)[1:4]<-c("Phyla", "Beginning", "End", "P")
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
+#Check name of columns to see what data looks like.
+names(rawdata)
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
+#An initial plot showing the composition of phyla at the beginning of the feedlot period in High RFI steers.
+ggplot(rawdata,aes(x="", y=Beginning, fill=Phyla))+ geom_bar(width = 1, stat = "identity")
 
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-  mutate_all(type.convert) %>% 
-  dplyr::filter(Height > 50 & Weight < 1000)
+#Save the plot to a file 
+
+#An initial plot showing the composition of phyla at the beginning of the feedlot period in High RFI steers.
+ggplot(rawdata,aes(x="", y=End, fill=Phyla))+ geom_bar(width = 1, stat = "identity")
+
+#Save the plot to a file
+
+#Load 
 
 #save data as RDS
 saveRDS(cleandata, file = "./data/processed_data/processeddata.rds")
